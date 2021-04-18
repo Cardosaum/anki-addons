0bfef129dec2975fdb42ac85cb71bd848f964780
M. Cardoso <cardosaum@pm.me>
2021-04-18 18:40:47 -0300

    fix string formating due to python version incompatibility

--- hide_new_cards_until_next_day/__init__.py
+++ hide_new_cards_until_next_day/__init__.py
@@ -14,60 +14,72 @@ from aqt.qt import *
 from anki import Collection
 
 # import constant
-# from anki.consts import QUEUE_TYPE_MANUALLY_BURIED, QUEUE_TYPE_LRN
 from anki.consts import QUEUE_TYPE_MANUALLY_BURIED
 from aqt import gui_hooks
-# from time import time_ns
+from datetime import datetime
+from time import time_ns
+import re
 
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
-
-# LUCAS_TAG = "z.lucas_tag_hidden."
-
-
-# def time_marker() -> str:
-#     return str(time_ns())
+MARKER_SEP = '_'
+MARKER_TAG_BASE = 'zTagHidden' + MARKER_SEP
+REGEX_TAG = re.compile(f'^{MARKER_TAG_BASE}')
 
 
-# def lucas_tag() -> str:
-#     return f'{LUCAS_TAG}{time_marker()}'
+def marker_today():
+    d = datetime.fromtimestamp(time_ns()//10**9)
+    d = datetime(d.year, d.month, d.day, 0)
+    return d
 
 
-def bury_custom_cards_lucas(*args, **kargs) -> None:
-    ids = mw.col.find_cards("is:new -is:suspended added:1")
-    # mw.col.tags.bulkAdd(ids, lucas_tag())
-    for id in ids:
-        card = mw.col.getCard(id)
-        card.queue = QUEUE_TYPE_MANUALLY_BURIED
-        card.flush()
+def marker_tag() -> str:
+    return f'{MARKER_TAG_BASE}{marker_today().strftime("%Y-%m-%d")}'
+
+
+def is_marker_tag_from_today(tag: str) -> bool:
+    if tag != marker_tag():
+        return False
+    return True
+
+
+def remove_mark_tags(note, tags: list) -> None:
+    for tag in tags:
+        if is_marker_tag_from_today(tag):
+            continue
+        if REGEX_TAG.search(tag):
+            note.delTag(tag)
+        # TODO: remove after lucas use it
+        if re.search(r'^z\.lucas', tag):
+            note.delTag(tag)
+
+
+def custom_bury_cards(*args, **kargs) -> None:
+    ids = mw.col.find_notes("is:new -is:suspended added:1")
+    mw.col.sched.suspendCards(ids)
+    mw.col.tags.bulkAdd(ids, marker_tag())
     mw.reset()
 
 
-# def unbury_custom_cards_lucas(*args, **kargs) -> None:
-#     tag = lucas_tag()
-#     ids = mw.col.find_cards(f'tag:"{LUCAS_TAG}*" -is:new')
-#     mw.col.tags.bulkRem(ids, lucas_tag())
-#     for id in ids:
-#         card = mw.col.getCard(id)
-#         card.queue = QUEUE_TYPE_LRN
-#         card.flush()
-#         note = card.note()
-#         note.rem
-#     mw.reset()
+def custom_unbury_cards(*args, **kargs) -> None:
+    ids = mw.col.find_cards(f'tag:"{MARKER_TAG_BASE}*"')
+    for id in ids:
+        card = mw.col.getCard(id)
+        note = card.note()
+        remove_mark_tags(note, note.tags)
+        note.flush()
+    mw.reset()
 
 
-def lucas_main(*args, **kargs) -> None:
-    bury_custom_cards_lucas()
-    # unbury_custom_cards_lucas()
+def marker_main(*args, **kargs) -> None:
+    custom_bury_cards()
+    custom_unbury_cards()
 
 
-# execute when overview refresh
-# gui_hooks.overview_did_refresh.append(testFunction)
-# gui_hooks.deck_browser_will_render_content.append(testFunction)
-gui_hooks.add_cards_did_add_note.append(lucas_main)
-# create a new menu item, "test"
+gui_hooks.add_cards_did_add_note.append(marker_main)
+# create a new menu item, "Organizar Cartões"
 action = QAction("Organizar Cartões", mw)
 # set it to call testFunction when it's clicked
-qconnect(action.triggered, lucas_main)
+qconnect(action.triggered, marker_main)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
